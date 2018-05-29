2952a48722ec7b28ebb6c2e7803dd7502657d197
Chris Haddad <haddadc@karux.net>
2018-05-28 22:04:22 -0400

    remove log from invoke

--- scripts/step07-invoke.sh
+++ scripts/step07-invoke.sh
@@ -1,5 +1,5 @@
 cd ./src/github.com/karux/testRESTService
-sls invoke --function hello --path ./data/data.json -log 
-sls invoke --function world --path ./data/data.json -log
-sls invoke --function iface --path ./data/iface.json -log
+sls invoke --function hello --path ./data/data.json # -log 
+sls invoke --function world --path ./data/data.json # -log
+sls invoke --function iface --path ./data/iface.json # -log
 # -log
